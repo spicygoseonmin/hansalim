21959537f43a28a53a2e0ec81d923581d8aa8dad
seonmin <seonmin1414@gmail.com>
2024-09-13 15:34:00 +0900

    비주얼 베너 js, 비주얼 코드 수정(화면 오류 해결), 헤더 컬러 #fff 으로 설정

--- js/script.js
+++ js/script.js
@@ -1,53 +1,52 @@
 window.addEventListener("load", function () {
-    // data.json을 로딩, 연결시킨다.
-    const xhttp = new XMLHttpRequest();
-    //    console.log(xhttp);
-    xhttp.onreadystatechange = function (e) {
-      const req = e.target;
-      // console.log(req);
-  
-      if (req.readyState === XMLHttpRequest.DONE) {
-        const str = req.response;
-        console.log(str);
-        //   json문자열로 변화 JSON.parse(str)
-        let obj = JSON.parse(str);
-        //   console.log(obj);
-        VISUAL_ARR = obj.visual;
+  // data.json을 로딩, 연결시킨다.
+  const xhttp = new XMLHttpRequest();
+  //    console.log(xhttp);
+  xhttp.onreadystatechange = function (e) {
+    const req = e.target;
+    // console.log(req);
+
+    if (req.readyState === XMLHttpRequest.DONE) {
+      const str = req.response;
+      console.log(str);
+      //   json문자열로 변화 JSON.parse(str)
+      let obj = JSON.parse(str);
+      //   console.log(obj);
+      VISUAL_ARR = obj.visual;
       //   오늘의 물품
-  
-        //   console.log(VISUAL_ARR);
-        // ================
-        showVisual(); // 비주얼을 화면에 배치
-        //오늘의 물품 화면에 배치
-      }
-    };
-    //   자료호출
-    xhttp.open("GET", "data.json");
-    xhttp.send();
-    //   ============================================
-    // 비주얼 슬라이드==
-    let VISUAL_ARR;
-    let visualTag = this.document.getElementById("data-visual");
-    //   오늘의 물품
-    // ==============================================
-    // 비주얼 화면 출력 기능
-    function showVisual() {
-      let html = "";
-      VISUAL_ARR.forEach(function (item) {
-        //   console.log(item);
-        const tag = `
-      <div class="swiper-slide">
-                  <div class="visual-slide-page">
-                    <a href="${item.link}">
-                      <img src="images/${item.pic}" alt="${item.name}">
-                    </a>
-                  </div>
-                 </div>
-      `;
-        html += tag;
-      });
-      visualTag.innerHTML = html;
+
+      //   console.log(VISUAL_ARR);
+      // ================
+      showVisual(); // 비주얼을 화면에 배치
+      //오늘의 물품 화면에 배치
     }
+  };
+  //   자료호출
+  xhttp.open("GET", "data.json");
+  xhttp.send();
+  //   ============================================
+  // 비주얼 슬라이드==
+  let VISUAL_ARR;
+  let visualTag = this.document.getElementById("data-visual");
+  //   오늘의 물품
+  // ==============================================
+  // 비주얼 화면 출력 기능
+  function showVisual() {
+    let html = "";
+    VISUAL_ARR.forEach(function (item) {
+      //   console.log(item);
+      const tag = `
+    <div class="swiper-slide">
+                <div class="visual-slide-page">
+                  <a href="${item.link}">
+                    <img src="images/${item.pic}" alt="${item.name}">
+                  </a>
+                </div>
+               </div>
+    `;
+      html += tag;
+    });
+    visualTag.innerHTML = html;
     // swiper 기능 ====================
     const swVisual = new Swiper(".sw-visual", {
       loop: true, // loop : 무한으로 도는 것.
@@ -65,6 +64,20 @@ window.addEventListener("load", function () {
         type: "fraction",
       },
     });
-    //   ==========================end
-  });
-  
+// 비주얼 슬라이드 머춤 기능
+const swVisualPlay = document.querySelector(".visual-play")
+swVisualPlay.addEventListener("click" , function(){
+  if(swVisualPlay.classList.contains("active")){
+    swVisual.autoplay.start()
+    swVisualPlay.classList.remove("active")
+  }else{
+    swVisual.autoplay.stop()
+    swVisualPlay.classList.add("active")
+
+  }
+})
+
+  } // = showVisual end =
+
+  //   ==========================end
+});
